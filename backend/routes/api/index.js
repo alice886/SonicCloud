5d8f10f4127c5c620fb61e897327804f1fa233c9
alice886 <baconhill4tmr@gmail.com>
2022-06-28 16:20:06 -0700

    phase 1"

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,19 +1,26 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body })
-})
-
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
 
 
 //{"XSRF-Token":"WrrIzk3h-XAUiLTIoZ2FB7qs5741RDiPA2wk"}
 
+/*
+phase 1 - Test the API Router
+--> http://localhost:8000/api/csrf/restore
+fetch('/api/test', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "XSRF-TOKEN": "oq1xt43l-jq_UTuQpCWFOneEa9YqT9M-4vxY"
+    },
+    body: JSON.stringify({ hello: 'world' })
+  }).then(res => res.json()).then(data => console.log(data));
 
-
-
-
-
+*/
 
 
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,6 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-const apiRouter = require('./api');
-apiRouter.use('/api', apiRouter);   // All the URLs of the routes in the api router will be prefixed with /api.
 
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -17,6 +15,9 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
+
+const apiRouter = require('./api');
+router.use('/api', apiRouter);   // All the URLs of the routes in the api router will be prefixed with /api.
 
 
 
