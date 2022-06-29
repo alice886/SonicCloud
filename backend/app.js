a2be88c409fce1c9542cd7948af6cbaf0802d98f
alice886 <baconhill4tmr@gmail.com>
2022-06-28 17:34:23 -0700

    phase 2

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,40 @@ app.use(
 const routes = require('./routes');
 app.use(routes);
 
+// phase 2 -- Error Handling
+// 1/3 Resource Not Found Error-Handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+// 2/3 Sequelize Error-Handler
+const { ValidationError } = require('sequelize');
+app.use((err, _req, _res, next) => {
+    // app.use((err, req, res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map(e => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+// 3/3 Error Formatter Error-Handler
+app.use((err, _req, res, _next) => {
+    // app.use((err,req,res,next)=>{
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
+
+
 
 
 
