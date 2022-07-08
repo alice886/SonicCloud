07cff4cedfbcb2010c91e4de35e6a6942bad7af6
alice886 <baconhill4tmr@gmail.com>
2022-07-07 23:06:16 -0700

    change login logout

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const { sequelize } = require('../db/models')
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/database.js')[env];
