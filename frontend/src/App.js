f7c78c53e19de6b8d3a6403943b0c0dbfb66056b
alice886 <baconhill4tmr@gmail.com>
2022-07-22 14:30:28 -0700

    front setup -b

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,9 +26,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
+          {/* <Route path="/login">
             <LoginFormPage />
-          </Route>
+          </Route> */}
           <Route path="/signup">
             <SignupFormPage />
           </Route>
