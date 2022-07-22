a656830ad5cc70b18c906aff0f78d15f824dccca
alice886 <baconhill4tmr@gmail.com>
2022-07-22 12:57:14 -0700

    frontend bug fix -1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,29 @@
+// function App() {
+//   return (
+//     <h1>Hello from App</h1>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from './store/session'
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded && (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
