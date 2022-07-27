5f2437bae30fc1745883c2ee8b77574088b28900
alice886 <baconhill4tmr@gmail.com>
2022-07-26 23:25:17 -0700

    reducer done - next:components/index.js

---
+++ frontend/src/components/Song/songDetails.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Link, Route, useParams, useHistory } from "react-router-dom";
+import { getOneAlbum, getAllAlbums } from '../../store/album'
+import { deleteOneAlbum, editOneAlbum } from '../../store/album';
+
+function SongDetails() {
+    return null;
+}
+
+export default SongDetails;
