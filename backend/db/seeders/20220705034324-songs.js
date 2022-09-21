0d59177ea60eaa773eba8f97d772c6ada2ebba28
alice886 <baconhill4tmr@gmail.com>
2022-09-20 23:14:37 -0700

    need to fix bottom player

--- backend/db/seeders/20220705034324-songs.js
+++ backend/db/seeders/20220705034324-songs.js
@@ -8,7 +8,7 @@ module.exports = {
         albumId: 2,
         title: 'Umbrella',
         description: 'This is a song by Barbadian singer Rihanna. It features American rapper Jay-Z.',
-        url: 'www.unbrella.song',
+        url: 'https://soniccloud886.s3.amazonaws.com/1663729705412.mp3',
         previewImage: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Good_Girl_Gone_Bad.png',
       },
       {
@@ -16,7 +16,7 @@ module.exports = {
         albumId: 2,
         title: 'Take A Bow',
         description: 'This is a song by Barbadian singer Rihanna. It features American rapper Jay-Z.',
-        url: 'www.unbrella.song',
+        url: 'https://soniccloud886.s3.amazonaws.com/1663729705412.mp3',
         previewImage: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Good_Girl_Gone_Bad.png',
       },
       {
@@ -24,7 +24,7 @@ module.exports = {
         albumId: 1,
         title: 'Empire State of Mind',
         description: 'This is a song performed by American rapper Jay-Z featuring vocals by American singer Alicia Keys',
-        url: 'www.empire.state.of.mind',
+        url: 'https://soniccloud886.s3.amazonaws.com/1663729705412.mp3',
         previewImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Empire_State_of_Mind_single_cover.jpg/220px-Empire_State_of_Mind_single_cover.jpg'
       },
       {
@@ -32,7 +32,7 @@ module.exports = {
         albumId: 1,
         title: 'Izzo (H.O.V.A.)',
         description: 'This song is often referred to as "H to the Izzo" is the first single released by Jay-Z from his sixth album The Blueprint. It is among his most popular singles.',
-        url: 'www.encore.song',
+        url: 'https://soniccloud886.s3.amazonaws.com/1663729705412.mp3',
         previewImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Empire_State_of_Mind_single_cover.jpg/220px-Empire_State_of_Mind_single_cover.jpg'
       },
       {
@@ -40,7 +40,7 @@ module.exports = {
         albumId: 3,
         title: 'No One',
         description: 'This is a song by American singer Alicia Keys from her third studio album, As I Am (2007).',
-        url: 'www.no.one',
+        url: 'https://soniccloud886.s3.amazonaws.com/1663729705412.mp3',
         previewImage: 'https://upload.wikimedia.org/wikipedia/en/0/0a/Alicia_Keys_-_As_I_Am.png'
       },
       {
@@ -48,7 +48,7 @@ module.exports = {
         albumId: 3,
         title: 'You Dont Know My Name',
         description: 'This is a song by American singer Alicia Keys from her third studio album, As I Am (2007).',
-        url: 'www.you.dont.know.my.name',
+        url: 'https://soniccloud886.s3.amazonaws.com/1663729705412.mp3',
         previewImage: 'https://upload.wikimedia.org/wikipedia/en/0/0a/Alicia_Keys_-_As_I_Am.png'
       }
     ], {});
