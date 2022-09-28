6bf8be6f4b3d0d2d6b868a8829e0c634a9f75547
alice886 <baconhill4tmr@gmail.com>
2022-09-28 13:40:25 -0700

    edit

--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { addComment, getSongComments } from "../../store/comment";
+import { useHistory, useParams } from "react-router-dom";
+import { addComment, getSongComments, editComment, deleteComment } from "../../store/comment";
 
 function SongComments({ songId }) {
     const dispatch = useDispatch();
+    const history = useHistory();
     const comments = useSelector(state => Object.values(state.comment));
     const sessionUser = useSelector(state => state.session.user);
     const [inputComment, setInputComment] = useState();
+    const [editComment, setEditComment] = useState();
+    const [commentSelected, setCommentSelected] = useState();
+    const [confrimDelete, setConfirmDelete] = useState(false);
+    const [errors, setErrors] = useState([]);
 
     useEffect(() => {
         dispatch(getSongComments(songId))
     }, [dispatch])
 
     const d = new Date();
-    const egtime = new Date('2022-09-28T04:57:41.000Z');
+    // const egtime = new Date('2022-09-28T04:57:41.000Z');
 
     const calTime = (time) => {
         const dateTime = new Date(time);
@@ -43,23 +48,94 @@ function SongComments({ songId }) {
             userId: sessionUser.id,
             body: inputComment
         }
-        dispatch(addComment(payload));
-        // if (hello){
-        // }
+
+        setErrors([]);
+        dispatch(addComment(songId, payload))
+            .then((res) => {
+                history.push(`/songs/${songId}`);
+            })
+            .catch(
+                async (res) => {
+                    const data = await res.json();
+                    if (data && data.errors) {
+                        setErrors(data.errors)
+                    };
+                }
+            );
+
+    }
+
+    const getHrMi = timeString => {
+        let converted = new Date(timeString);
+        let hr = converted.getHours();
+        let min = converted.getMinutes();
+        return hr + ":" + min;
+    }
+
+    // const testtime = new Date("2022-09-28T18:59:33.464Z")
+    // console.log('just posted as ---- ', testtime.getHours())
+    // console.log('just posted as ---- ', testtime.getMinutes())
+    const handleDeleteConfirm = async e => {
+        e.preventDefault();
+        confrimDelete ? setConfirmDelete(false) : setConfirmDelete(true);
+        setCommentSelected(e.target.value);
+    }
+
+    console.log(commentSelected)
+
+    const handleEditComment = async e => {
+        e.preventDefault();
+        const payload = {
+            id: e.target.value,
+            body: editComment,
+        }
+
+        setErrors([]);
+        dispatch(addComment(payload))
+            .then((res) => {
+                history.push(`/songs/${songId}`);
+            })
+            .catch(
+                async (res) => {
+                    const data = await res.json();
+                    if (data && data.errors) {
+                        setErrors(data.errors)
+                    };
+                }
+            );
+
+    }
+
+
+    const handleDeleteComment = async e => {
+        e.preventDefault();
+        setErrors([]);
+        dispatch(deleteComment(e.target.value))
+            .then((res) => {
+                history.push(`/songs/${songId}`);
+            })
+            .catch(
+                async (res) => {
+                    const data = await res.json();
+                    if (data && data.errors) {
+                        setErrors(data.errors)
+                    };
+                }
+            );
 
     }
 
     return (
         <div>
             <form>
-                {/* <ul>
+                <ul>
                     {errors.map((error, idx) => {
                         if (error !== 'Invalid value') {
                             return <li key={idx}>{error}</li>
                         }
                     }
                     )}
-                </ul> */}
+                </ul>
                 <input
                     type="text"
                     placeholder="Write a comment"
@@ -69,20 +145,45 @@ function SongComments({ songId }) {
                     onChange={e => setInputComment(e.target.value)} />
                 <button onClick={handlePostComment}>Post</button>
             </form>
+            <div>{comments.length} comment(s)</div>
 
             {comments && comments.map(comment => {
-                return <div key={comment.id}>
-                    <div>
-                        {comment.User.username} at {(comment.updatedAt.slice(11, 16))}
+                if (comment?.User?.id === sessionUser?.id) {
+                    return <div key={comment.id} >
+                        <div>
+                            {comment?.User?.username} at {getHrMi(comment?.updatedAt)}
+                        </div>
+                        <div>
+                            {calTime(comment?.updatedAt)}
+                        </div>
+                        <div>
+                            {comment?.body}
+                        </div>
+                        <button value={comment.id} onClick={handleEditComment}>✏️</button>
+                        <button value={comment.id} onClick={handleDeleteConfirm}>🗑️</button>
+                        {confrimDelete && (commentSelected == comment.id) && <div className="delete-confirm-bubble" > Do you really want to remove this comment?
+                            <button>Cancel</button>
+                            <button>Yes</button>
+                        </div>}
                     </div>
-                    <div>
-                        {calTime(comment.updatedAt)}
-                    </div>
-                    <div>
-                        {comment.body}
-                    </div>
+                }
 
-                </div>
+            })}
+            {comments && comments.map(comment => {
+                if (comment?.User?.id !== sessionUser?.id) {
+                    return <div key={comment.id}>
+                        <div>
+                            {comment?.User?.username} at {getHrMi(comment?.updatedAt)}
+                        </div>
+                        <div>
+                            {calTime(comment?.updatedAt)}
+                        </div>
+                        <div>
+                            {comment?.body}
+                        </div>
+                    </div>
+                }
+
             })}
         </div>
     )
