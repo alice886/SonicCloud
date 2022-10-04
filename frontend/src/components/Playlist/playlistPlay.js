af0dfd2387b8d3b64682b3fdc0f9b32fc1d96645
alice886 <baconhill4tmr@gmail.com>
2022-10-03 17:56:21 -0700

    css good

--- frontend/src/components/Playlist/playlistPlay.js
+++ frontend/src/components/Playlist/playlistPlay.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Link, Route, useParams } from "react-router-dom";
 import { getOnePlaylist, deleteSonginPlaylist, editOnePlaylist } from '../../store/playlist'
-import fakewaves from '../../images/soundwaves.gif'
+import fakewaves from '../../images/soundwaves02.gif'
 import '../../css-package/playlist.css'
 
 function PlaylistsPlay({ playing, setPlaying, setSongName, setArtistName }) {
@@ -28,23 +28,32 @@ function PlaylistsPlay({ playing, setPlaying, setSongName, setArtistName }) {
     const [readyEdit, setReadyEdit] = useState(false);
     // const theList = useSelector(state => Object.values(state.playlist));
 
-    const handlePlaylistPlay = async (e, song) => {
-        e.preventDefault();
-        await setPlaying(e.target.value);
-        await setSongName(song.title);
-        await setArtistName(song.Artist.username);
-        let homePlayer = document.getElementById('botton-player-bar');
-        if (played && songSelect == song.id) {
-            homePlayer.pause();
-            setPlayed(false);
-            setSongSelect();
-        }
-        else {
-            homePlayer.load();
-            homePlayer.play();
-            setSongSelect(song.id)
-            setPlayed(true);
-        }
+    // const handlePlaylistPlay = async (e, song) => {
+    //     e.preventDefault();
+    //     await setPlaying(e.target.value);
+    //     await setSongName(song.title);
+    //     await setArtistName(song.Artist.username);
+    //     let homePlayer = document.getElementById('botton-player-bar');
+    //     if (played && songSelect == song.id) {
+    //         homePlayer.pause();
+    //         setPlayed(false);
+    //         setSongSelect();
+    //     }
+    //     else {
+    //         homePlayer.load();
+    //         homePlayer.play();
+    //         setSongSelect(song.id)
+    //         setPlayed(true);
+    //     }
+    // }
+
+    let playlistvid = document?.getElementById("playlist-player-bar");
+
+    if (theList?.Songs.length === 0) {
+        document.getElementById("playlistSidenav").style.width = "0px";
+        return (<div className="playplaylist-container" id="playlistSidenav" class="playlistSidenav">
+            -
+        </div>)
     }
 
     let playlistSongs = [];
@@ -52,11 +61,10 @@ function PlaylistsPlay({ playing, setPlaying, setSongName, setArtistName }) {
         playlistSongs.push(each.url)
     });
     let songload = theList?.Songs[0].url;
-    let playlistvid = document?.getElementById("playlist-player-bar");
 
     const handlePlayList = async e => {
         e.preventDefault();
-        document.getElementById("playlistSidenav").style.width = "280px";
+        document.getElementById("playlistSidenav").style.width = "290px";
         let fvid = document?.getElementById("playlist-player-bar");
         let playindex = 0;
         songload = theList?.Songs[playindex].url;
@@ -73,9 +81,14 @@ function PlaylistsPlay({ playing, setPlaying, setSongName, setArtistName }) {
         },)
     }
 
+
     const hidePlaylist = async e => {
         e.preventDefault();
         document.getElementById("playlistSidenav").style.width = "0";
+    }
+    const toShowPlaylist = async e => {
+        e.preventDefault();
+        document.getElementById("playlistSidenav").style.width = "290px";
     }
 
     if (playlistvid) {
@@ -83,28 +96,33 @@ function PlaylistsPlay({ playing, setPlaying, setSongName, setArtistName }) {
     }
 
     return listLoaded && (
-        <div className="playplaylist-container" id="playlistSidenav" >
-            {/* <div className="playplaylist-audio">
+        <div className='playPlaylist-component'>
+            <button onClick={handlePlayList} className='playPlaylistButt'>▶</button>
+            <div className="playplaylist-container" id="playlistSidenav" class="playlistSidenav">
+                {/* <div className="playplaylist-audio">
                 <audio controls loop className='botton-player' id='botton-player-bar'>
                     <source src={playing} type="audio/mp3"></source>
                 </audio>
             </div> */}
-            <div>Now Playing playlist : {theList?.name}</div>
-            <button onClick={handlePlayList}>Play this Playlist</button>
-            <img src={fakewaves} height={'90px'}></img>
-            <audio controls id='playlist-player-bar'>
-                <source src={songload} type="audio/mp3" ></source>
-            </audio>
-            {theList?.Songs?.length && <div className="mylist-right-yes">
-                {theList.Songs.map(each => {
-                    return <div key={each.id} className='playplaylist-each'>
-                        <div className="mylist-yes-details">
-                            <NavLink to={`/songs/${each?.id}`}>{each.title}</NavLink>
+                <a onClick={toShowPlaylist} class="playlist-closebtn">{'<'}</a>
+                <a href="javascript:void(0)" class="playlist-openbtn" onClick={hidePlaylist}>&times;</a>
+                <div>Now Playing playlist :
+                    <NavLink to={`/songs/${theList?.id}`}> {theList?.name}</NavLink></div>
+                <img src={fakewaves} height={'90px'}></img>
+                <audio controls id='playlist-player-bar'>
+                    <source src={songload} type="audio/mp3" ></source>
+                </audio>
+                {theList?.Songs?.length && <div className="mylist-right-yes-side">
+                    {theList.Songs.map(each => {
+                        return <div key={each.id} className='playplaylist-each'>
+                            <div className="mylist-yes-details">
+                                <NavLink to={`/songs/${each?.id}`}>🎵 &nbsp; {each.title}</NavLink>
+                            </div>
                         </div>
-                    </div>
-                })}
+                    })}
+                </div>
+                }
             </div>
-            }
         </div>
     )
 }
